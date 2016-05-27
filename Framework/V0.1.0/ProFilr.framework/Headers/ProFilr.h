2959b22af1a1c07f814e90590aad2d7d51f51ffb
Rahul  Belekar <rahulbelekar@Rahuls-MacBook-Pro.local>
2016-05-27 17:05:29 +0530

    Framework Version 0.1.0

---
+++ Framework/V0.1.0/ProFilr.framework/Headers/ProFilr.h
@@ -0,0 +1,19 @@
+//
+//  ProFilr.h
+//  ProFilr
+//
+//  Created by Rahul  Belekar on 04/05/16.
+//  Copyright © 2016 Rahul  Belekar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ProFilr.
+FOUNDATION_EXPORT double ProFilrVersionNumber;
+
+//! Project version string for ProFilr.
+FOUNDATION_EXPORT const unsigned char ProFilrVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProFilr/PublicHeader.h>
+
+
